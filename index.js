def6a5ceafffc655d59cabd662d0d8ca3aa300f5
zikarimov <zikarimov@mail.ru>
2024-10-07 15:37:37 +0500

    add backend-project

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,10 +19,30 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
+
+
+function currentTime (req, res, next) {
+  var paramDate = req.params.date;
+
+  if (!paramDate) req.date = new Date();
+  else req.date = isNaN(paramDate) ? new Date(paramDate) : new Date(Number(paramDate));
+  next();
+}
+
+app.get('/api/:date?', currentTime, function(req, res) {
+
+  if (isNaN(req.date.getTime())) return res.send({ error: "Invalid Date" });
+
+  res.send({
+      unix: req.date.getTime(),
+      utc: req.date.toUTCString()
+  });
+});
+
 
 
 
